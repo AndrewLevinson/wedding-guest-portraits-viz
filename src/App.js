f9cb7694e7d9a7de14f65000b2a5388985aba6cf
Andrew Levinson <andrewlevins@gmail.com>
2024-09-08 21:08:45 -0400

    trying to make progress on viz...ran out of time :(

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
+import { useState, useEffect } from 'react';
+import * as d3 from 'd3';
 import './App.css';
 import Viz from './Viz';
 import Legend from './Legend';
 import rawData from './data.json';
 
 function App() {
-  const selectedData = rawData[42];
+  const cleanData = rawData.filter(x => x['Brooklyn Wedding'] === 'Attending');
+  const distanceExtent = d3.extent(cleanData.map(x => x['Miles to get to Le Fanfare']));
+  const timeKnownExtent = d3.extent(cleanData.map(x => x['Years known to bride or groom']));
+  const [selectedPerson, setSelectedPerson] = useState(42);
+  const selectedData = cleanData[selectedPerson];
+
+  const options = cleanData.map((x, i) => {
+    return (
+      <option value={i} key={i}>
+        {x['First Name']}
+      </option>
+    );
+  });
+
   return (
-    <main className='main'>
-      <div className='content'>
-        <h1 className='header'>
-          {selectedData['First Name']} {selectedData['Last Name']}
-        </h1>
-        <Viz data={selectedData} />
-        <Legend />
-      </div>
-    </main>
+    <>
+      <select value={selectedPerson} onChange={e => setSelectedPerson(e.target.value)}>
+        {options}
+      </select>
+      <main className='main'>
+        <div className='content'>
+          <h1 className='header'>
+            {selectedData['First Name']} <br />
+            {selectedData['Last Name']}
+          </h1>
+          <Viz data={selectedData} distanceExtent={distanceExtent} timeKnownExtent={timeKnownExtent} />
+          <Legend />
+        </div>
+      </main>
+    </>
   );
 }
 
--- src/Viz/index.js
+++ src/Viz/index.js
@@ -1,19 +1,46 @@
 import './viz.css';
 import * as d3 from 'd3';
 
-function Viz({ data }) {
+function Viz({ data, timeKnownExtent, distanceExtent }) {
   console.log(data);
+  const width = 250;
+  const height = 250;
+  const isBridesSide = data?.["Bride's side"];
+  const isGroomSide = data?.["Groom's side"];
+  const both = data?.["Bride's side"] && data?.["Groom's side"];
+  // const fillColor = data?.["Bride's side"] ? 'red' : data?.["Groom's side"] ? 'black' : 'green';
+  const timeScale = d3.scaleLinear(timeKnownExtent, [height / 2 + height * 0.13, height]);
+  const distanceScale = d3.scaleLinear(distanceExtent, [height / 2 + height * 0.13, height]);
+
   return (
     <div className='svg-holder'>
-      <svg viewBox='0 0 250 250'>
+      <svg viewBox={`0 0 ${width} ${height}`}>
         <g transform='translate(1,1)'>
+          <line
+            x1={'50%'}
+            y1={'50%'}
+            x2={'50%'}
+            y2={distanceScale(data['Miles to get to Le Fanfare'])}
+            className='line'
+          ></line>
+          <line
+            x1={'50%'}
+            y1={'50%'}
+            x2={timeScale(data['Years known to bride or groom'])}
+            y2={'0%'}
+            className='distance-line'
+          ></line>
           <g className='rings'>
-            <circle cx='50%' cy='50%' r='40%' className='outer'></circle>
-            <circle cx='50%' cy='50%' r='35%' className='middle'></circle>
-            <circle cx='50%' cy='50%' r='30%' className='inner'></circle>
+            <circle cx='50%' cy='50%' r={width / 2 - 2} className='outer'></circle>
+            <circle cx='50%' cy='50%' r={width / 2.5 - 2} className='middle'></circle>
+            <circle cx='50%' cy='50%' r={width / 3.5 - 2} className='inner'></circle>
+            <circle cx='50%' cy='50%' r={width / 5.5 - 2} className='inner'></circle>
           </g>
           <g>
-            <circle cx='50%' cy='50%' r='10%' className='middle'></circle>
+            {isBridesSide && <circle cx='50%' cy='50%' r='10px' className='middle' fillOpacity={0.5}></circle>}
+            {isGroomSide && (
+              <rect x='50%' y='50%' width='10px' height='10px' className='middle' fillOpacity={0.5}></rect>
+            )}
           </g>
         </g>
       </svg>
